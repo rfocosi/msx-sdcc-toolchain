097729c712bc7912fdb2b4daa77a88fb138b2f54
Roberto Focosi <roberto.focosi@ifood.com.br>
2019-10-02 19:31:48 -0300

    Better version control on Docker hub

--- docker-push.sh
+++ docker-push.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-VERSION=$1
+HUB_REPOSITORY=rfocosi/msx-sdcc-toolchain
+VERSION=`grep TOOLCHAIN_VERSION Dockerfile | sed 's/.\+TOOLCHAIN_VERSION=//' | head -n1`
 
 if [ -z "$VERSION" ]; then
-  echo "Usage: `basename $0` <version>"
+  echo "ERROR: TOOLCHAIN VERSION not found!"
   exit 1
 fi
 
-docker build . -t msx-sdcc-toolchain
-docker tag msx-sdcc-toolchain rfocosi/msx-sdcc-toolchain:$VERSION
-docker push rfocosi/msx-sdcc-toolchain:$VERSION
+docker build -t $HUB_REPOSITORY:$VERSION .
+
+SDCC_VERSION=`docker run $HUB_REPOSITORY:$VERSION sdcc -v | head -n 1 | awk '{ print $4 }'`
+
+[ -z "$SDCC_VERSION" ] && echo "ERROR: Version empty!" && exit 1
+
+docker tag $HUB_REPOSITORY:$VERSION $HUB_REPOSITORY:$VERSION-SDCC-$SDCC_VERSION
+docker push $HUB_REPOSITORY:$VERSION-SDCC-$SDCC_VERSION
